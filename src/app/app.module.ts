3b637b6d0fac090618c21081ef7927afd6eac950
Daniel Valencia <daniel.valencia@salamancasolutions.com>
2017-05-04 15:35:34 -0400

    project ini with FB

---
+++ src/app/app.module.ts
@@ -0,0 +1,44 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { ErrorHandler, NgModule } from '@angular/core';
+import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { SplashScreen } from '@ionic-native/splash-screen';
+import { StatusBar } from '@ionic-native/status-bar';
+
+import { MyApp } from './app.component';
+import { HomePage } from '../pages/home/home';
+
+// Import the AF2 Module
+import { AngularFireModule } from 'angularfire2';
+ 
+// AF2 Settings
+export const firebaseConfig = {
+  apiKey: "AIzaSyBPNWjFwUVynwDrYQXLWG7VyMCpjoUcpeA",
+  authDomain: "feel-f7289.firebaseapp.com",
+  databaseURL: "https://feel-f7289.firebaseio.com",
+  projectId: "feel-f7289",
+  storageBucket: "feel-f7289.appspot.com",
+  messagingSenderId: "758704113987"
+};
+
+@NgModule({
+  declarations: [
+    MyApp,
+    HomePage
+  ],
+  imports: [
+    BrowserModule,
+    IonicModule.forRoot(MyApp),
+    AngularFireModule.initializeApp(firebaseConfig)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    {provide: ErrorHandler, useClass: IonicErrorHandler}
+  ]
+})
+export class AppModule {}
---
+++ src/pages/home/home.ts
@@ -0,0 +1,48 @@
+import { Component } from '@angular/core';
+import { NavController, AlertController } from 'ionic-angular';
+
+import {AngularFireDatabase, FirebaseListObservable} from 'angularfire2/database';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  songs: FirebaseListObservable<any>;
+
+  constructor(public navCtrl: NavController, public alertCtrl: AlertController, af: AngularFireDatabase) {
+    this.songs = af.list('/songs');
+  }
+
+  addSong(){
+    let prompt = this.alertCtrl.create({
+      title: 'Song Name',
+      message: "Enter a name for this new song you're so keen on adding",
+      inputs: [
+        {
+          name: 'title',
+          placeholder: 'Title'
+        },
+      ],
+      buttons: [
+        {
+          text: 'Cancel',
+          handler: data => {
+            console.log('Cancel clicked');
+          }
+        },
+        {
+          text: 'Save',
+          handler: data => {
+            this.songs.push({
+              title: data.title
+            });
+          }
+        }
+      ]
+    });
+    prompt.present();
+  }
+
+}
